7166880917a699e41b30aef87b54b49cfe50ffec
Diego Nina <dninas@unsa.edu.pe>
2026-07-18 13:07:41 -0500

    chore: preparar configuraciones para despliegue en Render

--- backend/run.py
+++ backend/run.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 if __name__ == "__main__":
     # Si no existen en el .env, usa estos valores por defecto
-    host = os.getenv("HOST", "127.0.0.1")
+    host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", 8000))
     
     print(f"Iniciando Aura-Slurry Backend en {host}:{port}")
